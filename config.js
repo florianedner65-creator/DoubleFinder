c6caaa534e0ff03b6cfa85be866328b5661825e3
florianedner65-creator <florianedner65@gmail.com>
2026-09-13 22:40:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 window.DF_CONFIG = {
   SUPABASE_URL: "https://alsemnsjooxylcdtngah.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_-Kip71RkXk1E1CVJ_44PAA_B7ChBnyH"
-};
+  SUPABASE_ANON_KEY: "sb_publishable_-Kip71RkXk1E1CVJ_44PAA_B7ChBnyH"};
